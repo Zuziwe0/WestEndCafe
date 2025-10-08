a5af41f8027ee80d001d07d381ce0e535ecfbc0b
Zuziwe Mthembu <4145861@myuwc.ac.za>
2025-10-08 14:56:58 +0200

    update WestEndCafe

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Cart
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'item', 'quantity', 'added_at')
+    list_filter = ('added_at',)
+    search_fields = ('user__username', 'item__name')
+    ordering = ('-added_at',)
+    readonly_fields = ('added_at',)
+    list_editable = ('quantity',)
+    raw_id_fields = ('user', 'item')
